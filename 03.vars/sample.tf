68eb8918ded6f70ad5a1b297920ea40cafd2e791
varun <varunannepu123@gmail.com>
2023-10-18 19:21:40 +0530

    Terraform variables

--- 03.vars/sample.tf
+++ 03.vars/sample.tf
@@ -8,4 +8,57 @@ output "fruit_name" {
 }
 
 # List of variable
+variables "fruits" {
+    default = [
+        "apples",
+        "bananas"
+    ]
+    #default = [ "apples" , "bananas" ]  // single line syntax
+}
+
+# Map Variables, Plain
+variables "fruits_stocks" {
+    default = {
+        apples = 100
+        bananas = 200
+    }
+}
+
+#Map Variables, Maps of map
+variables "fruits_name_with_stock_price" {
+    default = {
+     apples = {
+        stock = 100
+        price = 3
+    }
+     bananas = {
+        stock = 400
+        price = 1
+    }
+  }
+}
+
+#Access a list variable , list index starts with zero
+output "fruits" {
+    value = var.fruits[0]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
