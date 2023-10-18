0b6227081107c27284f1998f92de4a88cb56da6a
varun <varunannepu123@gmail.com>
2023-10-18 23:51:12 +0530

    Terraform variables

--- 04.functions/main.tf
+++ 04.functions/main.tf
@@ -3,5 +3,16 @@ variable "fruits" {
  }
 
 output "fruits" {
+    //value = var.fruits[2]
     value = element(var.fruits, 2)
+}
+
+variable "fruit_with_stock" {
+    default = {
+     apple = 100
+  }
+}
+
+output "fruit_with_stock" {
+    value = try(var.fruit_with_stock["banana"], 0)
 }
