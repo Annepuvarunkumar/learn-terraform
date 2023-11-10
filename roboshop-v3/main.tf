85e2918945e10f7042aae13b9fabcdd3c33cab3d
varun <varunannepu123@gmail.com>
2023-11-10 18:07:01 +0530

    Terraform

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -5,8 +5,8 @@ module "components" {
     source              = "./module"
     zone_id             = "var.zone_id"
     security_group      = "var.security_group"
-    name                = each.vale["name"]
-    instance_type       = each.vale["instance_type"]
+    name                = each.value["name"]
+    instance_type       = each.value["instance_type"]
 
 }
 
