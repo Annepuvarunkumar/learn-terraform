7dfe56a3da70dd09fc88cffaf29369ae0ddb968e
varun <varunannepu123@gmail.com>
2023-11-13 21:26:07 +0530

    Terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -2,8 +2,8 @@
 
 resource "aws_instance" "instance" {
   ami                      = data.aws_ami.ami.id
-  instance_type            = var.instance_type
-  vpc_security_group_ids   = var.security_group
+  instance_type            = "var.instance_type"
+  vpc_security_group_ids   = "var.security_group"
 
   tags = {
     name = var.name
